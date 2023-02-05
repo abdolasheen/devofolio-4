059dc210fd806e620ae297a34dc5cb923acaf5f5
abdolasheen <lashin.apdo@gmail.com>
2023-02-05 19:02:51 +0200

    devofolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ["Developer", "freelancer", "Photographer"],
+  typeSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: false,
+};
+
+var typed = new Typed(".typing", options);
